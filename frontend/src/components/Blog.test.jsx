0a0b73e24cf12b6dabe70e52f955bd76812f9b78
Matt Anis <webdev.omis@gmail.com>
2026-06-15 23:58:27 +0100

    5.14: Blog List Tests, step 2

--- frontend/src/components/Blog.test.jsx
+++ frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
 import { expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 test("<Blog /> shows only title initially", async () => {
   const blog = {
@@ -17,4 +18,24 @@ test("<Blog /> shows only title initially", async () => {
 
   expect(title).toBeVisible();
   expect(author).toBeNull();
+});
+
+test("<Blog /> shows likes, url, and author when button is clicked", async () => {
+  const blog = {
+    author: "me",
+    title: "only the title should be visible",
+    likes: 1,
+    url: "https://example.com",
+  };
+
+  render(<Blog blog={blog} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  const author = screen.queryByText("me");
+  const title = screen.getByText("only the title should be visible");
+  expect(title).toBeVisible();
+  expect(author).toBeVisible();
 });
