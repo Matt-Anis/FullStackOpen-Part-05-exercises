57c4fc5de69d8a0fb92d61b3b02d6693bd19e2e9
Matt Anis <webdev.omis@gmail.com>
2026-06-15 01:55:57 +0100

    5.7 Blog List Frontend, step 7

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleExpand = () => setExpanded(!expanded);
+
+  const like = () => {};
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={toggleExpand}>view</button>
+      {expanded && (
+        <div>
+          <p>{blog.url}</p>
+          Likes: {blog.likes}
+          <button onClick={like}>like</button>
+          <p>{blog.author}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
