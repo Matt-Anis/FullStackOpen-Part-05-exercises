e6e971cbb372700c507cb748c47b16159f11670f
Matt Anis <webdev.omis@gmail.com>
2026-06-17 22:51:29 +0100

    5.18: Blog List End To End Testing, step 2

--- e2e/blog_app.spec.js
+++ e2e/blog_app.spec.js
@@ -1,8 +1,17 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith } = require("./helper");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
     await page.goto("http://localhost:5173");
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
+      data: {
+        name: "Matt Anis",
+        username: "mattanis",
+        password: "secret",
+      },
+    });
   });
 
   test("Login form is shown", async ({ page }) => {
@@ -10,5 +19,15 @@ describe("Blog app", () => {
     await page.getByRole("button", { name: "login" }).click();
     await expect(page.getByRole("textbox", { name: "username" })).toBeVisible();
     await expect(page.getByRole("textbox", { name: "password" })).toBeVisible();
+  });
+
+  test("login succeeds with correct credentials", async ({ page }) => {
+    await loginWith(page, "mattanis", "secret");
+    await expect(page.getByText("Matt Anis logged in")).toBeVisible();
+  });
+
+  test("login fails with wrong credentials", async ({ page }) => {
+    await loginWith(page, "mattanis", "wrong");
+    await expect(page.getByText("wrong credentials")).toBeVisible();
   });
 });
---
+++ e2e/helper.js
@@ -0,0 +1,8 @@
+const loginWith = async (page, username, password) => {
+  await page.getByRole("button", { name: "login" }).click();
+  await page.getByRole("textbox", { name: "username" }).fill(username);
+  await page.getByRole("textbox", { name: "password" }).fill(password);
+  await page.getByRole("button", { name: "login" }).click();
+};
+
+export { loginWith };
