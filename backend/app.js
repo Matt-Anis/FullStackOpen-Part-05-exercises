1391e90e608714fe96df6aeb7dbde4a1d4175e84
Matt Anis <webdev.omis@gmail.com>
2026-06-20 02:08:26 +0100

    fix: track backend as regular directory

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const blogRouter = require("./controllers/blogs");
+const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+
+const app = express();
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
+
+app.use(express.json());
+app.use(middleware.tokenExtractor);
+app.use("/api/blogs", blogRouter);
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
